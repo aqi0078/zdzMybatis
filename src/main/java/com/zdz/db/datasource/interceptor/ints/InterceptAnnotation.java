a235334f155329d255831de620bbcec59be81741
dezhi <584638114@qq.com>
2020-03-19 19:02:14 +0800

    update拦截器

--- src/main/java/com/zdz/db/datasource/interceptor/ints/InterceptAnnotation.java
+++ src/main/java/com/zdz/db/datasource/interceptor/ints/InterceptAnnotation.java
@@ -1,18 +1,18 @@
-//package com.zdz.db.datasource.interceptor.ints;
-//
-//import java.lang.annotation.ElementType;
-//import java.lang.annotation.Retention;
-//import java.lang.annotation.RetentionPolicy;
-//import java.lang.annotation.Target;
-//
-///**
-// * @author zhangdezhi
-// * @date 2020-03-19
-// */
-//
-//@Target({ElementType.METHOD, ElementType.PARAMETER})
-//@Retention(RetentionPolicy.RUNTIME)
-//public @interface InterceptAnnotation {
-//    boolean flag() default  true;
-//
-//}
+package com.zdz.db.datasource.interceptor.ints;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author zhangdezhi
+ * @date 2020-03-19
+ */
+
+@Target({ElementType.METHOD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface InterceptAnnotation {
+    boolean flag() default  true;
+
+}
